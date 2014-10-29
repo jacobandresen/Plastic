e27026a5105c3f0f787bb7605c9fe4a0170f016b
Jacob Andresen <jacob.andresen@gmail.com>
2014-10-29 20:46:29 +0100

    indent + module layout

--- public/js/Plastic.js
+++ public/js/Plastic.js
@@ -1,78 +1,102 @@
-
-window.onload = function () {
+/*global $,document,alert,fabric*/
 
 function Plastic() {
-  var data = {
-     tables: [],
-     lines: []
-  };
 
-  var canvas = this.__canvas = new fabric.Canvas('c', {
-       selection: false
-   });
-   fabric.Object.prototype.originX = fabric.Object.prototype.originY = 'center';
+    var canvas = new fabric.Canvas('c', { selection: false }),
+        data = {
+            tables: [],
+            lines: []
+        },
+        exports = {};
 
-   function makeTable(x,y) {
-       var c = new fabric.Rect({
-                 left: x,
-                 top: y,
-                 width: 50,
-                 height: 100,
-                 strokeWidth: 1,
-                 fill: '#fff',
-                 stroke: 'black'
-       });
-       c.hasControls = c.hasBorders = false;
-       c.on('selected', function (option) {
-           console.log("selected:%o", c);
-           option.e.stopPropagation();
-           option.e.cancelBubble=true;
-       });
+    fabric.Object.prototype.originX = fabric.Object.prototype.originY = 'center';
 
-       data.tables.push(c);
-                                                                                                   return c;
-       function makeRelation(id1, id2) {
-           var line = new fabric.Line(coords, {
-                                      fill: 'red',
-                                      stroke: 'black',
-                                      strokeWidth: 1,
-                                      selectable: false
-                      });
-           data.lines.push(line);
-           return line;
-       }
+    function addTable(table) {
+        table.on('selected', function (option) {
+            //make sure that we do not add a new table here
+            option.e.stopPropagation();
+            option.e.cancelBubble = true;
+        });
 
-       function init () {
-           canvas.on('mouse:down', function (options) {
-               console.log("mouse:down:%o", options);
-               canvas.add(makeTable(
-                   options.e.clientX,
-                   options.e.clientY)
-               );
-              options.e.stopPropagation();
-              options.e.cancelBubble=true;
-                                                                                                                                                   });             
-              canvas.on('object:moving', function (e) {
-                  var p = e.target;
-                  canvas.renderAll();
-              });
+        data.tables.push(table);
+        canvas.add(table);
+    }
 
-       }
+    function createTable(x,y) {
+       var table = new fabric.Rect({
+            left       : x,
+            top        : y,
+            width      : 50,
+            height     : 100,
+            strokeWidth: 1,
+            fill       : '#fff',
+            stroke     : 'black'
+        });
+        table.hasControls = false;
+        table.hasBorders = false;
+        return table;
+    }
 
-       exports= {};
-       exports.save = function () {
-           alert(data);
-       };
+    function loadData(data) {
+        this.data = data;
+        $(this.data.tables).each(function (obj) { addTable(obj); });
+    }
 
-       $('#save').on('click', function () {
-          exports.save();
-                                                                                                    });
-        init();
-      return exports;
+    function getTable(id) {
+        return data.tables[id];
+    }
+
+    function createRelation(idFrom, idTo) {
+        var from   = getTable(idFrom),
+            to     = getTable(idTo),
+            coords = [from.left, from.top, to.left, to.top],
+            line   = new fabric.Line(coords, {
+                                  fill: 'red',
+                                  stroke: 'black',
+                                  strokeWidth: 1,
+                                  selectable: false
+                   });
+        return line;
+    }
+
+    function addRelation(relation) {
+        data.lines.push(relation);
+        canvas.add(relation);
+    }
+
+    function registerEvents() {
+        canvas.on('mouse:down', function (options) {
+            addTable(createTable(options.e.clientX, options.e.clientY));
+
+            options.e.stopPropagation();
+            options.e.cancelBubble = true;
+            canvas.renderAll();
+        });
+
+        //canvas.on('object:moving', function (e) {
+        //   var p = e.target;
+        //   canvas.renderAll();
+        //});
+
+        $(this.saveButtton).on('click', function () {
+            exports.save();
+        });
+    }
+
+    registerEvents();
+
+    exports.addTable       = addTable;
+    exports.createRelation = createRelation;
+    exports.addRelation    = addRelation;
+    exports.loadData       = loadData;
+
+    exports.save = function () {
+        alert(data);
     };
 
- };
-
-    console.log("startup");
-    var p = new Plastic();
+    return exports;
 }
+
+$(document).ready(function () {
+    new Plastic();
+});
